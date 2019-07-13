4929235ef8702d05092103f8682284f4afe76c76
rotem <nevgauker@gmail.com>
2019-07-13 19:05:18 +0300

    update categories.

--- api/controllers/categories.js
+++ api/controllers/categories.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 exports.getCategories = (request, response, next) => {
-                response.status(200).send({categories: ["clothing & accessories", "books, movies & games", "cosmetics & body care", "food & drinks", "household","garden & pets", "other"]});
+                response.status(200).send({categories: ["All","Clothing","Accessories","Books","Movies", "Games", "Cosmetics", "Food", "Drinks", "Household","Garden","Pets", "Other"]});
 
 
 };
